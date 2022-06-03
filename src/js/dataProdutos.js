8725c95ebc87371bb5bc1166bbdbc73a11d9cee3
Pedro Gabriel <pedrogv03@hotmail.com>
2022-06-03 15:01:08 -0300

    implentação de novas funcionalidades

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,58 +1,123 @@
-const produtos = [
-    {
+const produtos = [{
+        id: 1,
         nome: 'Banana',
-        preco: 2,
+        preco: 2.00,
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: "./src/img/banana.png"
+        img: "./src/img/banana.png",
+        promocao: true,
+        precoPromocao: 1.00,
+        componentes: [
+            'Potássio',
+            'Vitamina B6',
+            'Vitamina C',
+            'Folatos'
+        ]
     },
     {
+        id: 2,
         nome: 'Morango',
-        preco: 2,
+        preco: 2.00,
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/morango.png'
+        img: './src/img/morango.png',
+        promocao: false,
+        precoPromocao: '0',
+        componentes: [
+            'Fibras',
+            'Vitamina C',
+            'Cálcio',
+            'Ferro'
+        ]
     },
     {
+        id: 3,
         nome: 'Maçã',
-        preco: 2,
+        preco: 2.00,
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/maca.png'
+        img: './src/img/maca.png',
+        promocao: true,
+        precoPromocao: 1.50,
+        componentes: [
+            'Potássio',
+            'Vitamina A',
+            'Vitamina E',
+            'Vitamina C'
+        ]
     },
     {
+        id: 4,
         nome: 'Pão',
-        preco: 4,
+        preco: 4.00,
         secao: 'Panificadora',
         categoria: 'Pães',
-        img: './src/img/pao.png'
+        img: './src/img/pao.png',
+        promocao: true,
+        precoPromocao: 2.50,
+        componentes: [
+            'Cálcio',
+            'Sódio',
+            'Fibra Alimentar',
+            'Proteínas'
+        ]
     },
     {
+        id: 5,
         nome: 'Leite',
-        preco: 5,
-        secao: 'Laticínio',
+        preco: 5.00,
+        secao: 'Laticinio',
         categoria: 'Leite',
-        img: './src/img/leite.png'
-    },
-    {
+        img: './src/img/leite.png',
+        promocao: false,
+        precoPromocao: 1.00,
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais',
+            "Proteínas"
+        ]
+    }, {
+        id: 6,
         nome: 'Bolo',
-        preco: 10,
+        preco: 10.00,
         secao: 'Panificadora',
         categoria: 'Doces',
-        img: './src/img/bolo.png'
+        img: './src/img/bolo.png',
+        componentes: [
+            'Sódio',
+            'Fibra Alimentar',
+            'Proteínas',
+            'Gorduras totais'
+        ]
     },
     {
+        id: 7,
         nome: 'Torta de maça',
-        preco: 4,
+        preco: 4.00,
         secao: 'Panificadora',
         categoria: 'Doces',
-        img: './src/img/tortaMaca.png'
+        img: './src/img/tortaMaca.png',
+        componentes: [
+            'Cálcio',
+            'Sódio',
+            'Fibra Alimentar',
+            'Proteínas'
+        ]
     },
     {
+        id: 8,
         nome: 'Yorgut de Morango',
-        preco: 3,
+        preco: 3.00,
         secao: 'Panificadora',
         categoria: 'Doces',
-        img: './src/img/yorgutDeMorango.png'
+        img: './src/img/yorgutDeMorango.png',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais',
+            "Proteínas"
+        ]
     }
+
 ]
